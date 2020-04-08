3015a7f84d0f60e05d84ef3c604dafdaf880ced0
Demonick2010 <kolyan20022002@gmail.com>
2020-04-08 20:47:57 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Init vars
+            int sumRange = 0;
+            string readedFile = string.Empty;
+            int convertedIntFile;
+            int answer = 0;
+
+            // Init paths
+            string inputFilePath = $"Input.txt";
+            string outputFilePath = $"Output.txt";
+
+            // Init random
+            Random rnd = new Random(DateTime.Now.Millisecond);
+
+            // Give random number МАХ 10^4
+            sumRange = rnd.Next(-10000, 10000);
+
+            // Delete files if exists
+            if (File.Exists(inputFilePath))
+                File.Delete(inputFilePath);
+            if (File.Exists(outputFilePath))
+                File.Delete(outputFilePath);
+
+            // Create new input file and write random number in txt file
+            using (FileStream fileStream = new FileStream(inputFilePath, FileMode.OpenOrCreate))
+            {
+                // Encoding string in bytes array
+                byte[] result = System.Text.Encoding.Default.GetBytes(sumRange.ToString());
+                // Write bytes in txt file
+                fileStream.Write(result, 0, result.Length);
+            }
+
+            // Show formatted number
+            WriteLine($"Sum range is '1' - '{sumRange:N0}'");
+
+            // Open and read txt file
+            using (FileStream fileStream = File.OpenRead(inputFilePath))
+            {
+                // init byte array
+                byte[] array = new byte[fileStream.Length];
+                // Read number from file
+                fileStream.Read(array, 0, array.Length);
+
+                // Engoding byte array to string
+                readedFile = System.Text.Encoding.Default.GetString(array);
+
+                // Convert string to int
+                convertedIntFile = Convert.ToInt32(readedFile);
+            }
+
+            // !!!!!!!!!!!!!Count up sum WRONG DECISION!!!!!!!!!!!!!
+            //for (int i = 1; i <= convertedIntFile; i++)
+            //{
+            //    answer += i;
+            //}
+            // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+            // Count up sum CORRECT DECISION
+            if (convertedIntFile > 0)
+                answer = (1 + convertedIntFile) * convertedIntFile / 2;
+            else
+                answer = (-1 + convertedIntFile) * convertedIntFile / 2;
+
+            // Show formatted sum in console
+            WriteLine($"Sum is: '{answer:N0}'");
+
+            // Write sum in output txt file
+            using (FileStream fileStream = new FileStream(outputFilePath, FileMode.OpenOrCreate))
+            {
+                // Init bayt array and encode output from int to byte
+                byte[] result = System.Text.Encoding.Default.GetBytes(answer.ToString());
+                // Write in output file
+                fileStream.Write(result, 0, result.Length);
+            }
+        }
+    }
+}
+
+//В этой задаче можно воспользоваться форумулой арифметической прогрессии: Sn = (a1+an)*n/2. Но начинающие программисты часто забывают эту формулу и в этом случае прибегают к
+//вычислению этой суммы с помощью цикла и решают эту задачу, используя следующий алгоритм:
+
+//read(n);
+//s=0;
+//for i=1..n{
+//s=s+i;
+//}
+//write(s);
+
+//Но сложность этой задачи не в вычислении этой суммы и приведенный выше алгоритм не проходит на тесте! Оказывается, здесь очень внимательно нужно прочитать условия задачи,
+//особенно ограничения на число N. Дело в том, что это число может быть отрицательным!!!
---
+++ GameTask/Program.cs
@@ -0,0 +1,145 @@
+﻿//В свободное время одноклассники Вася и Петя любят играть в различные логические игры: морской бой, крестики-нолики, шахматы, шашки и многое другое. Ребята уже испробовали и поиграли
+//во всевозможные классические игры подобного рода, включая компьютерные. Однажды им захотелось сыграть во что-нибудь новое, но ничего подходящего найти не удалось.
+//Тогда Петя придумал следующую игру «Угадайка»: Играют двое участников. Первый загадывает любое трехзначное число, такое что первая и последняя цифры отличаются
+//друг от друга более чем на единицу. Далее загадавший число игрок переворачивает загаданное число, меняя первую и последнюю цифры местами, таким образом получая еще одно число.
+//Затем из максимального из полученных двух чисел вычитается минимальное. Задача второго игрока – угадать по первой цифре полученного в результате вычитания числа само это число.
+//Например, если Вася загадал число 487,
+//то перестановкой первой и последней цифры он получит число 784. После чего ему придется вычесть из 784 число 487, в результате чего получится число 297, которое и должен отгадать Петя
+//по указанной первой цифре «2», взятой из этого числа. Петя успевает лучше Васи по математике, поэтому практически всегда выигрывает в играх такого типа. Но в данном случае Петя схитрил и
+//специально придумал такую игру, в которой он не проиграет Васе в любом случае. Дело в том, что придуманная Петей игра имеет выигрышную стратегию, которая заключается в следующем: искомое
+//число всегда является трехзначным и вторая его цифра всегда равна девяти, а для получения значения последней достаточно отнять от девяти первую, т.е. в рассмотренном выше случае последняя
+//цифра равна 9-2=7. Помогите Пете еще упростить процесс отгадывания числа по заданной его первой цифре, написав соответствующую программу.
+
+using System;
+using static System.Console;
+
+
+namespace GameTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstDigit = 0, convertedDigit = 0, trueDigit = 0, finalAnswer = 0;
+            GameLogic gl = new GameLogic();
+
+            firstDigit = gl.PrintStartMenu();
+            convertedDigit = gl.ChangeDigits(firstDigit);
+            trueDigit = gl.TrueDigit(firstDigit, convertedDigit);
+            finalAnswer = gl.Game(trueDigit);
+
+            ForegroundColor = ConsoleColor.Green;
+            WriteLine($"\nPlayer 1 number is: {trueDigit}, \nyour or computer answer is: {finalAnswer}\nCongratulation! Player 2 win!");
+            ResetColor();
+        }
+    }
+
+    public class GameLogic
+    {
+        public int PrintStartMenu()
+        {
+            while (true)
+            {
+                Write("Player 1, please, write 3-digit number (first digit must be larger or smaller of the last digit): ");
+                int digit = Convert.ToInt32(ReadLine());
+
+                if ((digit > 99) && (digit < 999))
+                {
+                    if (digit.ToString()[0] == digit.ToString()[2])
+                    {
+                        ForegroundColor = ConsoleColor.DarkRed;
+                        WriteLine("First digit must be larger or smaller of the last digit. Please, try again!\n");
+                        ResetColor();
+                    }
+                    else
+                    {
+                        Clear();
+                        return digit;
+                    }
+                }
+                else
+                {
+                    ForegroundColor = ConsoleColor.DarkRed;
+                    WriteLine($"numbers must be in the range: 100 - 998. Please, try again!\n");
+                    ResetColor();
+                }
+            }
+        }
+
+        public int ChangeDigits(int _playerOneNumber)
+        {
+            char[] tempNum = _playerOneNumber.ToString().ToCharArray();
+            Array.Reverse(tempNum);
+            string answer = new string(tempNum);
+                
+            return Convert.ToInt32(answer);
+        }
+
+        public int TrueDigit(int _firstDigit, int _convertedDigit)
+        {
+            if (_firstDigit > _convertedDigit)
+                return _firstDigit - _convertedDigit;
+            else
+                return _convertedDigit - _firstDigit;
+        }
+
+        public int Game(int _trueDigit)
+        {
+            while (true)
+            {
+                int userAnswer = 0;
+
+                ForegroundColor = ConsoleColor.DarkCyan;
+                WriteLine($"Guess the number by the first digit! First digit is: {_trueDigit.ToString()[0]}");
+                Write($"Guess yourself? (Yes = 'Y', No = 'N') if answer 'NO', the computer will guess itself! :");
+                ResetColor();
+
+                string tempAnswer = ReadKey().KeyChar.ToString().ToLower();
+                char lowerAnswer = Convert.ToChar(tempAnswer);
+
+                switch (lowerAnswer)
+                {
+                    case 'y':
+                    {
+                        Write($"\n\nInsert full number (first digit is {_trueDigit.ToString()[0]}): ");
+                        userAnswer = Convert.ToInt32(ReadLine());
+
+                        if (userAnswer == _trueDigit)
+                        {
+                            return userAnswer;
+                        }
+                        else
+                        {
+                            ForegroundColor = ConsoleColor.DarkRed;
+                            WriteLine($"\nWrong! Try again.\n");
+                            ResetColor();
+                        }
+
+                        break;
+                    }
+                    case 'n':
+                    {
+                        ForegroundColor = ConsoleColor.DarkGreen;
+                        WriteLine($"\n\n1. First digit is {_trueDigit.ToString()[0]}\n" +
+                                  $"2. Second digit always matters 9\n" +
+                                  $"3. Third number formula is {_trueDigit.ToString()[1]} - {_trueDigit.ToString()[0]} = {_trueDigit.ToString()[2]}\n");
+                        ResetColor();
+
+                        int computerAnswerThirdNum = Convert.ToInt32(_trueDigit.ToString()[1]) -
+                                                     Convert.ToInt32(_trueDigit.ToString()[0]);
+                        int compAnswer = Convert.ToInt32($"{_trueDigit.ToString()[0]}9{computerAnswerThirdNum}");
+
+                        return compAnswer;
+                    }
+                    default:
+                    {
+                        ForegroundColor = ConsoleColor.DarkRed;
+                        WriteLine($"\nError! The answer can only be 'Y' or 'N'! Try again!\n"); 
+                        ResetColor();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
